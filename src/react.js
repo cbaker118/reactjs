020d9d470c0350c74621178c555e034028d2a0c8
Christopher Aue <christopheraue@googlemail.com>
2011-04-16 18:20:25 +0200

    added website to license

--- src/react.js
+++ src/react.js
@@ -1,6 +1,6 @@
 /*
  * 
- * Copyright (c) 2011 Christopher Aue
+ * Copyright (c) 2011 Christopher Aue, http://reactjs.com
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
